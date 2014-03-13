e57ffaa52a59633bdf0b1bb640d760c79e7a5e69
João Xavier <jcxavier@jcxavier.com>
2014-03-13 02:41:55 +0100

    handling nicely the repopulation of the buffer when the drawableobject is cleaned. also cleared the constants a little bit up

---
+++ engine/src/main/java/com/jcxavier/android/opengl/util/Constants.java
@@ -0,0 +1,18 @@
+package com.jcxavier.android.opengl.util;
+
+/**
+ * Created on 13/03/2014.
+ *
+ * @author João Xavier <jcxavier@jcxavier.com>
+ */
+public final class Constants {
+
+    /**
+     * The float size, in bytes.
+     */
+    public static final int FLOAT_SIZE = 4;
+
+    private Constants() {
+        // can't be instantiated
+    }
+}
