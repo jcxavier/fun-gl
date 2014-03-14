79c437bf015e7f594373a19694cd1cc0760edb57
João Xavier <jcxavier@jcxavier.com>
2014-03-14 10:30:54 +0100

    updated the sample to reflect alpha and visibility changing

--- sample/src/main/java/com/jcxavier/android/opengl/sample/TestStage.java
+++ sample/src/main/java/com/jcxavier/android/opengl/sample/TestStage.java
@@ -17,10 +17,18 @@ import com.jcxavier.android.opengl.math.Vector3;
  */
 public class TestStage extends SimpleGameStage {
 
-    private GameObject movingObject;
+    private long mFrame;
+
+    private DrawableObject mClickableShape;
+    private GameObject mMovingObject;
+
+    private int mAlphaSign;
 
     public TestStage(final Camera camera) {
         super(camera);
+
+        mFrame = 0;
+        mAlphaSign = 1;
     }
 
     @Override
@@ -28,17 +36,17 @@ public class TestStage extends SimpleGameStage {
         super.onLoad();
 
         // create a static shape that resets the moving object upon click
-        DrawableObject clickableShape = new DrawableObject();
+        mClickableShape = new DrawableObject();
 
         // top-left of the object should be at (400, 400)
-        clickableShape.setPosition(new Vector3(450, 450, 0));
-        clickableShape.setSize(new Vector2(100, 100));
-        clickableShape.setAnchorPoint(new Vector2(0.5f, 0.5f));
+        mClickableShape.setPosition(new Vector3(450, 450, 0));
+        mClickableShape.setSize(new Vector2(100, 100));
+        mClickableShape.setAnchorPoint(new Vector2(0.5f, 0.5f));
 
-        clickableShape.setColor(new Vector3(0.5f, 0.5f, 1.0f));
-        clickableShape.setAlpha(0.7f);
+        mClickableShape.setColor(new Vector3(0.5f, 0.5f, 1.0f));
+        mClickableShape.setAlpha(0.7f);
 
-        clickableShape.setInputHandler(new InputHandler() {
+        mClickableShape.setInputHandler(new InputHandler() {
             @Override
             public boolean processTouch(final MotionEvent event) {
                 resetMovingShapePosition();
@@ -46,35 +54,51 @@ public class TestStage extends SimpleGameStage {
             }
         });
 
-        movingObject = new Sprite("spaceship.png");
-        movingObject.setAnchorPoint(new Vector2(0.0f, 0.0f));
-        movingObject.setScale(new Vector3(1.0f, 1.5f, 1.0f));
-        movingObject.setAlpha(0.4f);
+        mMovingObject = new Sprite("spaceship.png");
+        mMovingObject.setAnchorPoint(new Vector2(0.0f, 0.0f));
+        mMovingObject.setScale(new Vector3(1.0f, 1.5f, 1.0f));
+        mMovingObject.setAlpha(0.5f);
 
         // add the objects to the stage, they will be automatically managed and updated
-        addGameObject(movingObject);
-        addGameObject(clickableShape);
+        addGameObject(mMovingObject);
+        addGameObject(mClickableShape);
 
         // set the initial position of the moving shape
         resetMovingShapePosition();
     }
 
     private void resetMovingShapePosition() {
-        movingObject.setPosition(new Vector3(0, 0, 0));
+        mMovingObject.setPosition(new Vector3(0, 0, 0));
     }
 
     @Override
     public void onUpdate(double dt) {
         super.onUpdate(dt);
 
-        float moveOffset = (float) (dt * 20);
+        mFrame++;
+        float dtFloat = (float) dt;
+        float moveOffset = dtFloat * 20;
 
         // update the current moving shape position with the previously computed moveOffset
-        Vector3 currentPosition = movingObject.getPosition();
+        Vector3 currentPosition = mMovingObject.getPosition();
         currentPosition.x += moveOffset;
         currentPosition.y += moveOffset;
 
         // setting the position of the object will trigger the update of the transformations
-        movingObject.setPosition(currentPosition);
+        mMovingObject.setPosition(currentPosition);
+
+        // shift alpha per frame (should loop in 2.5s)
+        float alpha = mMovingObject.getAlpha() + mAlphaSign * dtFloat * 0.4f;
+        mMovingObject.setAlpha(alpha);
+
+        // reverse the sign once it is fully visible or invisible
+        if (alpha > 1.0f || alpha < 0.0f) {
+            mAlphaSign *= -1;
+        }
+
+        // trigger the button visibility (it shouldn't trigger while hidden)
+        if (mFrame % 200 == 0) {
+            mClickableShape.setVisible(!mClickableShape.isVisible());
+        }
     }
 }
